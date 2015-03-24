d5f79ba19d08ff1daae1950c407507c69b38ff9a
torreyn <torrey@nommesen.com>
2015-03-23 17:24:46 -0700

    using API instead of fake data

--- src/js/app.js
+++ src/js/app.js
@@ -2,74 +2,15 @@
 
     var app = angular.module('movieSearcher', []);
 
-    app.controller('SearchController', function () {
-        // alert('SearchController() loaded');
-        this.results = movies.Search;
-    });
+    app.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
+        console.log('SearchController loaded');
+        
+        $http.get('http://www.omdbapi.com/?s=last&r=json').success(function (data) {
+            $scope.data = data.Search;
+        }).error(function (data) {
+            console.log('error with API callback');
+        });
 
-    var movies = {
-        "Search": [
-            {
-                "Title": "Indiana Jones and the Last Crusade",
-                "Year": "1989",
-                "imdbID": "tt0097576",
-                "Type": "movie"
-            },
-            {
-                "Title": "X-Men: The Last Stand",
-                "Year": "2006",
-                "imdbID": "tt0376994",
-                "Type": "movie"
-            },
-            {
-                "Title": "The Last Samurai",
-                "Year": "2003",
-                "imdbID": "tt0325710",
-                "Type": "movie"
-            },
-            {
-                "Title": "The Last King of Scotland",
-                "Year": "2006",
-                "imdbID": "tt0455590",
-                "Type": "movie"
-            },
-            {
-                "Title": "The Last Airbender",
-                "Year": "2010",
-                "imdbID": "tt0938283",
-                "Type": "movie"
-            },
-            {
-                "Title": "The Last Stand",
-                "Year": "2013",
-                "imdbID": "tt1549920",
-                "Type": "movie"
-            },
-            {
-                "Title": "The Last of the Mohicans",
-                "Year": "1992",
-                "imdbID": "tt0104691",
-                "Type": "movie"
-            },
-            {
-                "Title": "Avatar: The Last Airbender",
-                "Year": "2005–2008",
-                "imdbID": "tt0417299",
-                "Type": "series"
-            },
-            {
-                "Title": "I Know What You Did Last Summer",
-                "Year": "1997",
-                "imdbID": "tt0119345",
-                "Type": "movie"
-            },
-            {
-                "Title": "Last Action Hero",
-                "Year": "1993",
-                "imdbID": "tt0107362",
-                "Type": "movie"
-            }
-        ]
-    };
-
+    }]);
+    
 })();
