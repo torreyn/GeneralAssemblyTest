29d95080b3c3091c35d0e3a0e9a0148cefa9ae55
torreyn <torrey@nommesen.com>
2015-03-23 20:39:45 -0700

    full functionality

--- src/js/app.js
+++ src/js/app.js
@@ -5,14 +5,37 @@
     app.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
         console.log('SearchController loaded');
         
+        // search movie
         $scope.runSearch = function() {
             console.log($scope.query);
+            
             
             $http.get('http://www.omdbapi.com/?s=' + $scope.query + '&r=json').success(function (data) {
                 $scope.data = data.Search;
             }).error(function (data) {
-                console.log('error with API callback');
+                console.log('error calling API');
             });
         }
+        
+        // movie found, show plot and info
+        $scope.findMovie = function(movieTitle){
+            console.log('findMovie() called with ' + movieTitle);
+            
+            $http.get('http://www.omdbapi.com/?t=' + movieTitle + '&plot=long&r=json').success(function(data) {
+                console.log(data);
+                $scope.movieReturned = data;
+                $scope.data = '';
+            }).error(function(data) {
+                console.log('error calling API');
+            });
+        };
+        
+        $scope.reset = function(){
+            console.log('reset() called');
+            $scope.movieReturned = '';
+            
+        };
+        
+        
     }]);
 })();
