fc420c085ba1a0f0f1194c95ff4cf2468ba58ac8
torreyn <torrey@nommesen.com>
2015-03-23 17:52:36 -0700

    search bar functional

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,14 @@
     app.controller('SearchController', ['$scope', '$http', function ($scope, $http) {
         console.log('SearchController loaded');
         
-        $http.get('http://www.omdbapi.com/?s=last&r=json').success(function (data) {
-            $scope.data = data.Search;
-        }).error(function (data) {
-            console.log('error with API callback');
-        });
-
+        $scope.runSearch = function() {
+            console.log($scope.query);
+            
+            $http.get('http://www.omdbapi.com/?s=' + $scope.query + '&r=json').success(function (data) {
+                $scope.data = data.Search;
+            }).error(function (data) {
+                console.log('error with API callback');
+            });
+        }
     }]);
-    
 })();
